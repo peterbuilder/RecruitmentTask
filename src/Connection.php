bfef19496cdb30e47029b607f1903e997e304cc3
peterbuilder <peterbuilder@github.com>
2017-03-16 16:18:04 +0100

    Add __construct() and function query()

--- src/Connection.php
+++ src/Connection.php
@@ -3,5 +3,39 @@
 
 class Connection
 {
+    private $mysqli;
+    private $insert_id;
 
+    public function __construct()
+    {
+        require 'config.php';
+
+        try {
+            $this->mysqli = new mysqli($host, $user, $password, $database);
+
+            if($this->mysqli->connect_error) {
+                throw new Exception("Can't connect with database");
+            }
+
+            $this->mysqli->set_charset('utf8');
+        }
+        catch (Exception $e) {
+            echo $e->getMessage() . ' -> ';
+            echo $this->mysqli->connect_error;
+        }
+    }
+
+    public function query($sql)
+    {
+        $result = $this->mysqli->query($sql);
+
+        try {
+            if($result == false) {
+                throw new Exception("Query error");
+            }
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 }
