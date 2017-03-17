c91eb6d7a7078da4eb71d5087e28225e66d4e776
peterbuilder <peterbuilder@github.com>
2017-03-17 13:53:07 +0100

    Add __construct()

--- src/User.php
+++ src/User.php
@@ -2,5 +2,21 @@
 
 class User
 {
+    private $id;
+    private $name;
+    private $surname;
+    private $email;
+    private $password;
+
+    public function __construct()
+    {
+        $this->id = -1;
+        $this->name = '';
+        $this->surname = '';
+        $this->email = '';
+        $this->password = '';
+    }
+
+
 
 }
