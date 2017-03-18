d59e7bca1694f9cc414cbabea5fa2e7318b3aa54
peterbuilder <peterbuilder@github.com>
2017-03-18 13:33:11 +0100

    Add authorization process for logging

---
+++ signIn.php
@@ -0,0 +1,29 @@
+<?php
+
+function __autoload($classname)
+{
+    $filename = 'src/' . $classname . '.php';
+    include_once $filename;
+}
+
+try {
+    if($_SERVER['REQUEST_METHOD'] != 'POST') {
+        throw new Exception("Datas have to be send by POST. ");
+    }
+
+    if($_POST['email'] == null || $_POST['password'] == null) {
+        throw new Exception("You should fill all text boxes. ");
+    }
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $authorization = new Authorization(new Connection());
+
+    if($authorization->signIn($email, $password)) {
+        echo 'Congratulation! You are logged in.';
+    } else {
+        echo 'Incorrect email or password. ';
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
